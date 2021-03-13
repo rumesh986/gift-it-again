63d65c72ac73dadc7070bfc678701849c59ecbf3
Rumesh Sudhaharan <rumesh986@gmail.com>
2021-03-13 14:01:43 +0800

    add licensing

--- lib/Giveaway.dart
+++ lib/Giveaway.dart
@@ -1,3 +1,19 @@
+//	Copyright (C) 2021 Rumesh Sudhaharan, Sridharan Arvind Srinivasan
+//	This file is part of gift-it-again.
+// 
+//	gift-it-again is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+// 
+//	gift-it-again is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+// 
+//	You should have received a copy of the GNU General Public License
+//	along with gift-it-again.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 import 'Summary.dart';
 import 'DataStructures.dart';
